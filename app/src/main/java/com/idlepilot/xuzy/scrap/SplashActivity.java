986a4c88657b13cc63b3041a0ff5498d632703d1
Xuzy <xuzywozz@163.com>
2016-01-03 17:25:52 +0800

    加入初始动画

---
+++ app/src/main/java/com/idlepilot/xuzy/scrap/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.idlepilot.xuzy.scrap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Administrator on 2015/12/27.
+ */
+public class SplashActivity extends Activity {
+    private GifView startGif;
+
+    //等待gif播放完毕
+    private final int SPLASH_DISPLAY_LENGHT = 9600;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        startGif= (GifView) findViewById(R.id.startGif);
+        startGif.setMovieResource(R.raw.start);
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
